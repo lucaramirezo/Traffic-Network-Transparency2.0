a7aa301d1d893bd6efb3ecd89b7670bffefebe92
duo <duoxu1987@gmail.com>
2024-04-22 18:02:45 +0200

    Actualización último estado a estado entregado de Tika

--- practica2_Tika/main.py
+++ practica2_Tika/main.py
@@ -1,14 +1,16 @@
 from tikapp import TikaApp
 
+
 class Tika:
     detect_content_type = None
     detect_language = None
     extract_all = None
     extract_content = None
+
     def __init__(self, file_path):
         self.file_path = file_path
-        self.tika_client = TikaApp(file_jar='../tika-app-1.16.jar')
-        
+        self.tika_client = TikaApp(file_jar='../tika-app-1.16_duo.jar')
+
     def TikaReader(self):
         self.detect_content_type = self.tika_client.detect_content_type(self.file_path)
         print(self.detect_content_type)
@@ -18,12 +20,15 @@ class Tika:
         print(self.extract_all)
         self.extract_content = self.tika_client.extract_only_content(self.file_path)
         print(self.extract_content)
-        
+
     def ProcessJSONTika(json):
-        #Dado el JSON de procesamiento Tika escribir por consola los metadatos sobre
-        #autores, fecha de creación y fecha de modificación.
+        # Dado el JSON de procesamiento Tika escribir por consola los metadatos sobre
+        # autores, fecha de creación y fecha de modificación.
         pass
-    
+
+
 if __name__ == "__main__":
     tika = Tika('Guion_De_Eficacia_proyecto_final[1].docx')
     tika.TikaReader()
+
+#%%
